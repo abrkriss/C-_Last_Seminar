7b5f2bb198ad41e8207d62350c385c774264286b
Kristina <abrkriss@gmail.com>
2022-12-28 17:19:04 +0300

    Zadacha68

---
+++ Zadacha68/Program.cs
@@ -0,0 +1,18 @@
+﻿void Zadacha68()
+{
+int AkkermanFunc(int m, int n)
+		{
+			if(m == 0)
+			{
+				return n + 1;
+			}
+			if(m > 0 && n == 0)
+			{
+				return AkkermanFunc(m - 1, 1);
+			}
+			return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
+		}
+		
+		Console.WriteLine(AkkermanFunc(2,3));
+        }
+Zadacha68();
